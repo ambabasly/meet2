534ebea15a5bed8178e09e161428123880eb03f1
Sylvester Arikhan <arikhansylvester@gmail.com>
2021-10-07 23:14:15 +0200

    Correcting typos

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,9 +3,10 @@ import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
 describe('<NumberOfEvents /> component', () => {
+    
     let NumberOfEventsWrapper;
     beforeAll(() => {
-        NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+        NumberOfEventsWrapper = shallow(<NumberOfEvents updateEventCount={() => {}} />);
     });
 
     test('render number of events field', () => {
@@ -14,13 +15,13 @@ describe('<NumberOfEvents /> component', () => {
 
     test('render default 32 events', () => {
         expect(NumberOfEventsWrapper.find('.change-number')).toHaveLength(1);
-        expect(NumberOfEventsWrapper.state('eventsNumber')).toBe(32);
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
     });
 
     test('render change of state when text input is completed', () => {
         NumberOfEventsWrapper.setState({ eventsNumber: 32 });
         const eventsInput = { target: { value: 16 }};
         NumberOfEventsWrapper.find('.change-number').simulate('change', eventsInput);
-        expect(NumberOfEventsWrapper.state('eventsNumber')).toBe(16);
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(16);
     });
 });
